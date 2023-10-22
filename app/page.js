4d6c9b7c55afdc49b5fc90eced0f1ac5d486cf01
shahnewaz-gain <shahnewaz@gain.media>
2023-10-22 16:19:54 +0600

    implemented timeline calender

--- app/page.js
+++ app/page.js
@@ -1,113 +1,132 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useEffect, useMemo, useState } from "react";
+import { Eventcalendar, getJson, setOptions } from "@mobiscroll/react";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+import moment from "moment";
+import { ChevronDown } from "react-feather";
+import Image from "next/image";
+
+setOptions({
+  theme: "ios",
+  themeVariant: "light",
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [myEvents, setEvents] = useState([]);
+
+  const view = useMemo(
+    () => ({
+      timeline: {
+        type: "month",
+        resolutionHorizontal: "hour",
+        resolutionVertical: "day",
+        startTime: "10:00",
+        endTime: "21:00",
+        currentTimeIndicator: false,
+      },
+    }),
+    []
+  );
+
+  useEffect(() => {
+    getJson(
+      "https://trial.mobiscroll.com/timeline-vertical-events/",
+      (events) => {
+        setEvents(events);
+      },
+      "jsonp"
+    );
+  }, []);
+
+  const renderCustomHeader = () => (
+    <>
+      <div className="flex items-center gap-2 ml-8 my-5">
+        <Image
+          className="rounded-full shadow-lg"
+          src="/avatar.png"
+          alt="Bonnie image"
+          width={40}
+          height={40}
+        />
+        <div className="">
+          <div className="leading-6 flex justify-center items-center">
+            <span className="font-medium text-16 text-brand-secondary">
+              Agens Neilson
+            </span>
+            <ChevronDown className="w-6 h-6 font-normal text-brand-secondary ml-2" />
+          </div>
+          <p className="font-normal text-12 leading-0 text-border">
+            Laravel department
+          </p>
         </div>
       </div>
+      <div className="flex justify-between items-center gap-8 mr-16 my-5">
+        <div>
+          <p className="text-14 text-regular font-medium">40h 00m</p>
+          <p className="text-11 text-border">Scheduled</p>
+        </div>
+        <div>
+          <p className="text-14 text-regular font-medium">39h 30m</p>
+          <p className="text-11 text-border">Logged</p>
+        </div>
+        <div>
+          <p className="text-14 text-regular font-medium">-00h 30m</p>
+          <p className="text-11 text-border">Balance</p>
+        </div>
+        <div>
+          <p className="text-14 text-green font-medium">20h 30m</p>
+          <p className="text-11 text-border">Approved</p>
+        </div>
+        <div>
+          <p className="text-14 text-yellow font-medium">19h 30m</p>
+          <p className="text-11 text-border">Pending</p>
+        </div>
+      </div>
+    </>
+  );
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const renderCustomDay = (args) => (
+    <div className="py-2 pl-3">
+      <p className="text-border font-medium text-12">
+        {moment(args?.date).format("ddd")}
+      </p>
+      <p className="text-regular font-medium text-18 leading-7">
+        {moment(args?.date).format("D")}
+      </p>
+      <p className="font-normal text-12 text-border pt-2">8h 30m</p>
+    </div>
+  );
+
+  const renderCustomHour = (args) => (
+    <p
+      className="font-normal text-12 text-border"
+      style={{ lineHeight: "18px" }}
+    >
+      {moment(args?.date).format("H.mm")}
+    </p>
+  );
+
+  const renderEventContent = (arg) => {
+    console.log("arg", arg);
+
+    return <div className="bg-brand">asdasd</div>;
+  };
+
+  return (
+    <main className="px-24 timeline-calender">
+      <div className="m-8">
+        <Eventcalendar
+          theme="ios"
+          themeVariant="light"
+          view={view}
+          data={myEvents}
+          renderDay={renderCustomDay}
+          renderHour={renderCustomHour}
+          renderHeader={renderCustomHeader}
+          extendDefaultEvent={renderEventContent}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,139 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "1rem",
+        xl: "1rem",
+        "2xl": "1rem",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        stripes: {
+          "0%": {
+            backgroundPosition: "0 0",
+          },
+          "100%": {
+            backgroundPosition: "160px 0",
+          },
+        },
+      },
+      colors: {
+        brand: {
+          DEFAULT: "#2C67FF",
+          secondary: "#1A3AA1",
+          light: "#2C67FF33",
+        },
+        white: {
+          DEFAULT: "#FFF",
+          light: "#FBFAFF",
+        },
+        bar: {
+          blue: "#021457",
+          orange: "#f25100",
+          purple: "#8532a8",
+        },
+        yellow: {
+          DEFAULT: "#FFAB00",
+          light: "#FFF1D4",
+        },
+        red: {
+          DEFAULT: "#FE475B",
+          light: "#FFDADE",
+        },
+        green: {
+          DEFAULT: "#0CAA1B",
+          light: "#DAF2DC",
+        },
+        blue: {
+          DEFAULT: "#44A9F1",
+          light: "#E3F2FD",
+          soft: "#EDECFE",
+        },
+        gray: {
+          DEFAULT: "#F3F4F8",
+          blues: "#EDECFE",
+          deep: "#8F99AD",
+          light: "#EEF0F3",
+        },
+        border: {
+          DEFAULT: "#8D91A5",
+          light: "#D8E0ED",
+        },
+        orange: {
+          DEFAULT: "#FF834D",
+          light: "#FF834D40",
+        },
+        seagreen: {
+          DEFAULT: "#00B8D9",
+          light: "#00B8D933",
+        },
+        dark: "#21214E",
+        regular: "#5C5D76",
+        disabled: "#F8F9FB",
+        perple: "#5A49B4",
+        meganta: "#B43490",
+        brown: "#9B6546",
+        deepblue: "#2B82B4",
+      },
+      fontWeight: {
+        thin: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+      },
+      fontSize: {
+        11: "11px",
+        12: "12px",
+        13: "13px",
+        14: "14px",
+        16: "16px",
+        18: "18px",
+        20: "20px",
+        24: "24px",
+        32: "32px",
+      },
+      placeholderColor: {
+        gray: "#D8E0ED",
+      },
+      borderColor: {
+        gray: "#D8E0ED",
+      },
+      width: {
+        540: "540px",
+        265: "265px",
+        7.2: "34px",
+      },
+      height: {
+        590: "590px",
+        7.2: "34px",
+      },
+      boxShadow: {
+        "box-shadow": "0px 0px 40px 15px rgba(0, 0, 0, 0.15)",
       },
     },
   },
   plugins: [],
-}
+};
